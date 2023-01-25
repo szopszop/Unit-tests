8ce8de3e293c720c4e737e65b72beb8b6948bc62
szopszop <szymontracz1@gmail.com>
2023-01-25 12:17:32 +0100

    Class & Enum ParameterizedTest with Streams

--- src/test/java/pl/devfoundry/testing/MealTest.java
+++ src/test/java/pl/devfoundry/testing/MealTest.java
@@ -1,6 +1,14 @@
 package pl.devfoundry.testing;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -64,6 +72,38 @@ class MealTest {
         Meal meal = new Meal(8, "Soup");
         // then
         assertThrows(IllegalArgumentException.class, () -> meal.getDiscountedPrice(10));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {5, 10, 15, 19})
+    void mealPricesShouldBeLowerThan20(int price) {
+        assertThat(price, lessThan(20));
+    }
+
+    @ParameterizedTest
+    @MethodSource("createMealsWithNameAndPrice")
+    void burgerShouldHaveCorrectNameAndPrice(String name, int price) {
+        assertThat(name, containsString("burger"));
+        assertThat(price, greaterThanOrEqualTo(10));
+    }
+
+    private static Stream<Arguments> createMealsWithNameAndPrice() {
+        return Stream.of(
+                Arguments.of("Hamburger", 10),
+                Arguments.of("Cheeseburger", 12)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("createCakeNames")
+    void cakeNamesShouldEndWithCake(String name) {
+        assertThat(name, notNullValue());
+        assertThat(name, endsWith("cake"));
+    }
+
+    private static Stream<String> createCakeNames() {
+        List<String> cakeNames = Arrays.asList("Cheesecake", "Fruitcake", "Cupcake");
+        return cakeNames.stream();
     }
 
 }
