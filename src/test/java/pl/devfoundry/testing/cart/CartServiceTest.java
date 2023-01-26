75dd1d304fcb3dbb5856c8fffb51e0efd81b614f
szopszop <szymontracz1@gmail.com>
2023-01-26 10:06:44 +0100

    Verify, never() methods. BDD version with then(). should().

--- src/test/java/pl/devfoundry/testing/cart/CartServiceTest.java
+++ src/test/java/pl/devfoundry/testing/cart/CartServiceTest.java
@@ -10,6 +10,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 class CartServiceTest {
@@ -32,12 +33,39 @@ class CartServiceTest {
         verify(cartHandler).sendToPrepare(cart);
         verify(cartHandler, atLeastOnce()).sendToPrepare(cart);
 
+        // BDD version
+        then(cartHandler).should().sendToPrepare(cart);
+
         InOrder inOrder = inOrder(cartHandler);
         inOrder.verify(cartHandler).canHandleCart(cart);
         inOrder.verify(cartHandler).sendToPrepare(cart);
 
         assertThat(resultCart.getOrders(), hasSize(1));
         assertThat(resultCart.getOrders().get(0).getOrderStatus(), equalTo(OrderStatus.PREPARING));
+    }
+
+    @Test
+    void processCartShouldNotSendToPrepare() {
+        // given
+        Order order = new Order();
+        Cart cart = new Cart();
+        cart.addOrderToCart(order);
+        CartHandler cartHandler = mock(CartHandler.class);
+        CartService cartService = new CartService(cartHandler);
+
+        given(cartHandler.canHandleCart(cart)).willReturn(false);
+
+        // when
+        Cart resultCart = cartService.processCart(cart);
+
+        // then
+        verify(cartHandler, never()).sendToPrepare(cart);
+
+        // BDD version
+        then(cartHandler).should(never()).sendToPrepare(cart);
+
+        assertThat(resultCart.getOrders(), hasSize(1));
+        assertThat(resultCart.getOrders().get(0).getOrderStatus(), equalTo(OrderStatus.REJECTED));
     }
 
 
